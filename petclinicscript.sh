2831089a26f5034eefbb4c931f31baf31bb4aea2
ravishankarananth <ranantha@andrew.cmu.edu>
2017-05-30 23:12:00 -0400

    Add files via upload

--- petclinicscript.sh
+++ petclinicscript.sh
@@ -5,15 +5,16 @@ sudo apt-get -y update
 
 #install JAVA
 sudo apt-get -y install openjdk-8-jdk
+#set path of Java home
+sudo bash -c "echo 'JAVA_HOME=\"/usr/lib/jvm/java-8-openjdk-amd64\"' >> /etc/environment"
+
 #install GIT
 sudo apt-get -y install git
 #install Maven
 sudo apt-get -y install maven
-#set path of java home
-sudo bash -c "echo 'JAVA_HOME=\"/usr/lib/jvm/java-8-openjdk-amd64\"' >> /etc/environment"
 
 
 #clone petclinic and build
 sudo git clone https://github.com/spring-projects/spring-petclinic.git /home/spring-petclinic
 cd /home/spring-petclinic
-./mvnw spring-boot:run
+sudo ./mvnw spring-boot:run
