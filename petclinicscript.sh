603ad5d91bda176a1614c2045174eb689d31bdc1
ravishankarananth <ranantha@andrew.cmu.edu>
2017-05-30 23:00:07 -0400

    Add files via upload

--- petclinicscript.sh
+++ petclinicscript.sh
@@ -14,6 +14,6 @@ sudo bash -c "echo 'JAVA_HOME=\"usr/lib/jvm/java-8-openjdk-amd64\"' >> /etc/envi
 
 
 #clone petclinic and build
-git clone https://github.com/spring-projects/spring-petclinic.git /home/spring-petclinic
-cd spring-petclinic
+sudo git clone https://github.com/spring-projects/spring-petclinic.git /home/spring-petclinic
+cd /home/spring-petclinic
 ./mvnw spring-boot:run
